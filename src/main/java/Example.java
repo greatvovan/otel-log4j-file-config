cba617b2a9965fdabb60ef6cfedd4068f1199a75
Vladimir Ryabtsev <vryabtsev@news360.com>
2024-10-26 18:32:37 -0700

    Add NoClassDefFoundError issue repro

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,17 +1,11 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.common.CompletableResultCode;
+import io.opentelemetry.sdk.common.CompletableResultCode;   // Succeeds!
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
-import io.opentelemetry.instrumentation.log4j.appender.v2_17.OpenTelemetryAppender;
 
 public class Example {
-    private static final Logger logger = LogManager.getLogger(Example.class);
-
     public static void main(String[] args) throws java.lang.InterruptedException {
         OpenTelemetrySdk openTelemetry = AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
-        OpenTelemetryAppender.install(openTelemetry);
 
-        logger.info("My test log");
+        // openTelemetry.shutdown();  // Fixes tthe problem
     }
 }
